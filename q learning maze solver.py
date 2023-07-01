cbbebf043c47c25e76f7f4ad0171e9dd46cfc77e
burcukilic <94201593+burcukilic@users.noreply.github.com>
2023-07-01 16:42:34 +0300

    Update q learning maze solver.py

--- q learning maze solver.py
+++ q learning maze solver.py
@@ -1,170 +1,161 @@
-import pygame
-import random
 import math
+
+import pygame
+import numpy as np
+import random
+
+random.seed(1)
 pygame.init()
-win = pygame.display.set_mode((600,600))
+win = pygame.display.set_mode((600, 600))
 clock = pygame.time.Clock()
 run = True
-colors = [(255,235,240),(160,60,130),(0,255,0)]
-font = pygame.font.Font('freesansbold.ttf',25)
-Gen = 0
-dotx = 1
-doty = 1
+font = pygame.font.Font('freesansbold.ttf', 32)
 
-moves = []
-moves.append([random.randint(0,1),[-1,1][random.randint(0,1)]])
-moves.append([1,-1])
-tiles = [[2,2]]
+green = (0, 255, 0)
+red = (255, 0, 0)
+blue = (0, 0, 255)
+
+n = 5  # it will be an nxn square grid.
+
+# HYPERPARAMETERS
+learning_rate = 0.1
+discount_factor = 0.9
+epsilon = 1
+epsilon_decay = 0.99
+epsilon_min = 0.00
+
+goal = [4, 4]
+obstacle = [2, 2]
 
 
-
-class Rects:
-    def __init__(self,x,y,obstacle,value):
+# AGENT CLASS
+class Agent:
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.obstacle = obstacle
-        self.value = value
-    def draw(self,rect):
-        global Gen
-        global dotx
-        global doty
-        global tiles
-        self.rect = pygame.draw.rect(win,colors[self.obstacle],(self.x+1,self.y+1,59,59))
+        self.step = 0
+        self.obstacled = False
+        self.goalReached = False
 
-        #WRITING THE VALUES OF THE RECTS#
-        font2 = pygame.font.Font('freesansbold.ttf',10)
-        txt = font2.render(str(int(self.value)),True,(150,150,150))
-        win.blit(txt,(self.x,self.y+10))
+    def draw(self):
+        done = False
+        pygame.draw.circle(win, blue, (self.x * 600 / n + 300 / n, self.y * 600 / n + 300 / n), 10)
+        self.step += 1
+        if self.x == obstacle[0] and self.y == obstacle[1]:
+            self.obstacled = True
+            done = True
+        elif self.x == goal[0] and self.y == goal[1]:
+            self.goalReached = True
+            print(f"Goal Reached in {self.step} steps!")
+            done = True
+        elif self.x < 0 or self.x >= n or self.y < 0 or self.y >= n:
+            self.obstacled = True
+            done = True
+        elif self.step == max_steps:
+            done = True
 
-        if self.rect.collidepoint(dotx*60+30,doty*60+30):
-            #DYING#
-            if self.obstacle==1:
-                t = font.render("Dead",True,(255,0,0))
-                win.blit(t,(5,440))
-                grid[dotx][doty].value -= 10000
-                for i in range(len(tiles)):
-                    grid[tiles[i][0]][tiles[i][1]].value -= int(math.sqrt(i+1))
-                tiles = [[1,1]]
-                Gen += 1
-                dotx = 1
-                doty = 1
-            #WINNING#
-            if self.obstacle==2:
-                t = font.render('WON!!!', True, (0,0,255))
-                win.blit(t,(5,440))
-                grid[dotx][doty].value = 10000
-                for i in range(len(tiles)):
-                    grid[tiles[i][0]][tiles[i][1]].value +=1
-                tiles = [[1,1]]
-                dotx = 1
-                doty = 1
+        return done
 
+    def act(self, action):
+        if action == 0:
+            self.x -= 1
+        elif action == 1:
+            self.x += 1
+        elif action == 2:
+            self.y -= 1
+        elif action == 3:
+            self.y += 1
+
+    def fitness(self):
+        return -self.step - 60 * self.obstacled + 60 * self.goalReached - math.sqrt((goal[0]-self.x)**2 + (goal[1]-self.y)**2)
 
 
+num_actions = 4
 
+Q = np.random.uniform(low=-1, high=1, size=(n, n, num_actions))
 
-#CREATING THE GRID AND THE OBSTACLES#
-grid = [[0]*10 for i in range(10)]
-for i in range(10):
-    for j in range(10):
-        grid[i][j] = Rects(i*60,j*60,0,0)
-#######################
-grid[0][0].obstacle = 1
-grid[1][0].obstacle = 1
-grid[2][0].obstacle = 1
-grid[0][1].obstacle = 1
-grid[0][2].obstacle = 1
-grid[3][1].obstacle = 1
-grid[4][1].obstacle = 1
-grid[5][2].obstacle = 1
-grid[6][3].obstacle = 1
-grid[6][4].obstacle = 1
-grid[7][5].obstacle = 1
-grid[8][5].obstacle = 1
-grid[9][6].obstacle = 1
-grid[9][7].obstacle = 1
-grid[9][8].obstacle = 1
-grid[9][9].obstacle = 1
-grid[1][3].obstacle = 1
-grid[2][4].obstacle = 1
-grid[3][3].obstacle = 1
-grid[3][5].obstacle = 1
-grid[4][6].obstacle = 1
-grid[5][7].obstacle = 1
-grid[5][9].obstacle = 1
-grid[6][9].obstacle = 1
-grid[4][8].obstacle = 1
-grid[7][9].obstacle = 1
-grid[7][8].obstacle = 1
-grid[7][7].obstacle = 1
-grid[8][9].obstacle = 2
-#######################
+max_steps = 10
+current_step = 0
+episode_fitness = 0
+agent = Agent(0, 0)
+action = 0
+state = [0, 0]
+episode = 0
 
-
-a = 0
-
-
-
-
-#MAIN LOOP#
+# MAIN LOOP#
 while run:
-    #GENERAL STUFF#
-    win.fill((255,255,255))
+    # GENERAL STUFF#
+    win.fill((255, 255, 255))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    #DRAWING THE RECTANGLES AND THE LINES#
-    for i in range(10):
-        pygame.draw.line(win,(0,0,0),(i*60,0),(i*60,600))
-    for i in range(10):
-        pygame.draw.line(win,(0,0,0),(0,i*60),(600,i*60))
-    for i in range(10):
-        for j in range(10):
-            grid[i][j].draw(0)
-    
+    pygame.draw.rect(win, green, (goal[0] * 600 // n, goal[1] * 600 // n, 600 // n, 600 // n))
+    pygame.draw.rect(win, red, (obstacle[0] * 600 // n, obstacle[1] * 600 // n, 600 // n, 600 // n))
 
+    for i in range(n):
+        pygame.draw.line(win, (0, 0, 0), (i * 600 / n, 0), (i * 600 / n, 600))
+    for i in range(n):
+        pygame.draw.line(win, (0, 0, 0), (0, i * 600 / n), (600, i * 600 / n))
 
+    # ALGORITHM
 
-    #DRAWING THE CHARACTER AND MOVEMENTS
-    #brain.exe storming
-    values = [grid[dotx+1][doty].value,
-              grid[dotx-1][doty].value,
-              grid[dotx][doty+1].value,
-              grid[dotx][doty-1].value]
-    
-    pygame.draw.circle(win, (230,180,0) , (dotx*60+30,doty*60+30) , 10)
+    if current_step == 1:
+        agent = Agent(0, 0)
+        episode_fitness = 0
+        state = [0, 0]
 
-    
-    if values.count(max(values))>1:
-        ass = []
-        zz = 0
-        for h in range(values.count(max(values))):
-            zz = values.index(max(values),zz)
-            ass.append(zz)
-            zz+=1
-        n = ass[random.randint(0,values.count(max(values))-1)]
+    if np.random.rand() < epsilon:
+        actions = [0, 1, 2, 3]
+        if agent.x == 0:
+            actions.remove(0)
+        elif agent.x == n-1:
+            actions.remove(1)
+        if agent.y == 0:
+            actions.remove(2)
+        elif agent.y == n-1:
+            actions.remove(3)
+        action = random.choice(actions)
     else:
-        n = values.index(max(values))
-        
-    if n==0:
-        dotx+=1
-    elif n==1:
-        dotx-=1
-    elif n==2:
-        doty+=1
-    elif n==3:
-        doty-=1
+        actions = [0, 1, 2, 3]
+        if agent.x == 0:
+            actions.remove(0)
+        elif agent.x == n - 1:
+            actions.remove(1)
+        if agent.y == 0:
+            actions.remove(2)
+        elif agent.y == n - 1:
+            actions.remove(3)
+        maxValue = -100000
+        action = 0
+        for i in actions:
+            if Q[state[0], state[1], i] > maxValue:
+                maxValue = Q[state[0], state[1], i]
+                action = i
 
-    grid[dotx][doty].value -= 2
-    tiles.append([dotx,doty])
+    agent.act(action)
+    done = agent.draw()
 
+    next_state = [agent.x, agent.y]
 
+    Q[state[0], state[1], action] += learning_rate * (agent.fitness() + discount_factor * np.max(
+        Q[next_state[0], next_state[1]]) - Q[state[0], state[1], action])
 
-    #WRITING THE GEN AND FINAL DETAILS
-    text = font.render("Gen : " + str(Gen),True,(0,255,0))
-    win.blit(text,(5,560))
-    clock.tick(10)
+    episode_fitness += agent.fitness()
+    state = next_state
+
+    if done:
+        current_step = 0
+
+    current_step = current_step % 8 + 1
+
+    if current_step == 1:
+        if epsilon > epsilon_min:
+            epsilon *= epsilon_decay
+        episode += 1
+        print(f"Episode {episode}: Reward = {episode_fitness} Epsilon = {epsilon}")
+
+    clock.tick(5)
     pygame.display.update()
 
 pygame.quit()
